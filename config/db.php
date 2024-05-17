7b85ee70d7513eaa7548aacbce2cf04fb7514d5a
Anhcoder2107 <Anh.TN230234P@sis.hust.edu.vn>
2024-05-17 09:29:09 +0700

    Create file connect database

---
+++ config/db.php
@@ -0,0 +1,15 @@
+<?php
+
+class DB {
+    private static $instance = NULL;
+
+    private function __construct() {}
+
+    public static function getInstance() {
+        if (!isset(self::$instance)) {
+            $pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;
+            self::$instance = new PDO('mysql:host=localhost;dbname=database', 'root', 'root', $pdo_options);
+        }
+        return self::$instance;
+    }
+}
