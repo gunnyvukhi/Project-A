06eb417568c89440d6610ccf20a3a10cc60d96a1
guunyvukhi <gunnytuananh@gmail.com>
2024-05-20 09:21:13 +0700

    Da sua laij login

--- resources/js/login.js
+++ resources/js/login.js
@@ -15,50 +15,6 @@ function login() {
     }
 }
 
-function ReturnToSignIn(){
-    document.getElementById("loginContainer").style.display = 'none';
-    document.getElementById("SignInContainer").style.display = 'unset';
-    document.getElementById("forgotPasswordContainer").style.display = 'none';
-    document.getElementById("messageContainer").style.display = 'none';
-}
-
-
-const signInList = document.getElementsByClassName('signIn')
-for(let i = 0; i < signInList.length; i++) {
-    signInList[i].addEventListener("click", ReturnToSignIn);
-}
-
-function ReturnToLogIn(){
-    document.getElementById("loginContainer").style.display = 'unset';
-    document.getElementById("SignInContainer").style.display = 'none';
-    document.getElementById("forgotPasswordContainer").style.display = 'none';
-    document.getElementById("messageContainer").style.display = 'none';
-}
-
-
-const logInList = document.getElementsByClassName('logIn')
-for(let i = 0; i < logInList.length; i++) {
-    logInList[i].addEventListener("click", ReturnToLogIn);
-}
-
-function goToForgotPassword(){
-    document.getElementById("loginContainer").style.display = 'none';
-    document.getElementById("SignInContainer").style.display = 'none';
-    document.getElementById("forgotPasswordContainer").style.display = 'unset';
-    document.getElementById("messageContainer").style.display = 'none';
-}
-
-document.getElementById("forgotPassword").addEventListener("click", goToForgotPassword);
-
-let khoiPhucMatKhau = 0;
-function afterForgotPassword(){
-    if (khoiPhucMatKhau){
-        document.getElementById("forgotPasswordContainer").style.display = 'none';
-        document.getElementById("messageContainer").style.display = 'unset';
-    }else{
-        document.getElementById('passwordResult').innerHTML = 'Tài khoản email không hợp lệ';
-    }
-}
 
 
 
