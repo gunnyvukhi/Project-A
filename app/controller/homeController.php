469c5d09bae240c5a1c15df1e8a383b7f3b2d113
Anhcoder2107 <Anh.TN230234P@sis.hust.edu.vn>
2024-06-11 21:20:12 +0700

    update $data

--- app/controller/homeController.php
+++ app/controller/homeController.php
@@ -3,55 +3,12 @@
 require_once 'app/model/PostModel.php';
 require_once 'app/model/HiddenPostModel.php';
 require_once 'app/model/ActionLogModel.php';
+require_once 'app/controller/Controller.php';
 
 class HomeController {
     public function index() {
-        $PostModel = new PostModel();
-        $data = $PostModel->getAllPost();  
         
-        //check is_deleted
-        $data = array_filter($data, function($post){
-            return $post['is_deleted'] == 0;
-        });
-
-        //check hidden post with post_id
-        $HiddenPostModel = new HiddenPostModel();
-        $hiddenPosts = $HiddenPostModel->getHiddenPosts($_SESSION['userId']);
-
-        $data = array_filter($data, function($post) use ($hiddenPosts){
-            foreach($hiddenPosts as $hiddenPost){
-                if($post['post_id'] == $hiddenPost['post_id']){
-                    return false;
-                }
-            }
-            return true;
-        });
-        
-        //add comment to post with post_id
-        $data = array_map(function($post){
-            $PostModel = new PostModel();
-            $comments = $PostModel->getCommentByPostId($post['post_id']);
-            $post['comments'] = $comments;
-            return $post;
-        }, $data);
-
-        //check user has liked post
-        $data = array_map(function($post){
-            $PostModel = new PostModel();
-            $post['hasLiked'] = $PostModel->hasUserLikedPost($_SESSION['userId'], $post['post_id']);
-            return $post;
-        }, $data);
-
-
-        //get all action log by post_id
-        $ActionLogModel = new ActionLogModel();
-        $data = array_map(function($post){
-            $ActionLogModel = new ActionLogModel();
-            $actionLogs = $ActionLogModel->getActionLogByPostId($post['post_id']);
-            $post['actionLogs'] = $actionLogs;
-            return $post;
-        }, $data);
-
+        $data = Controller::Data();
 
         require_once 'resources\view\mainPage.php';
         
--- app/model/eventModel.php
+++ app/model/eventModel.php
@@ -4,7 +4,55 @@
 require_once 'config/db.php';
 
 class eventModel{
-    // event_id,	event_name,	event_description,	event_date,	event_time,	event_location,	event_type,	event_status,	event_image,	event_link,	event_organizer,	event_organizer_contact,	event_organizer_email,	event_organizer_website
+    // event_id,user_id, event_name,	event_date,	date_time
+    public function createEvent($user_id, $event_name, $event_date, $date_time){
+        $sql = "INSERT INTO event (user_id, event_name, event_date, date_time) VALUES ($user_id, '$event_name', '$event_date', '$date_time')";
+        $db = new DB;
+        $db = $db->query($sql);
+        return $db;
+    }
+
+    //insert events of vietnamese
+    // INSERT INTO important_events (event_name, event_date, event_description)
+    // VALUES
+    // ('Ngày thành lập Đảng Cộng sản Việt Nam', '1930-02-03', 'Ngày 3 tháng 2 năm 1930, Đảng Cộng sản Việt Nam được thành lập.'),
+    // ('Cách mạng tháng Tám', '1945-08-19', 'Ngày 19 tháng 8 năm 1945, nhân dân Hà Nội tiến hành khởi nghĩa giành chính quyền, mở đầu cho cuộc Tổng khởi nghĩa tháng Tám.'),
+    // ('Ngày Quốc khánh', '1945-09-02', 'Ngày 2 tháng 9 năm 1945, Chủ tịch Hồ Chí Minh đọc Tuyên ngôn Độc lập, khai sinh nước Việt Nam Dân chủ Cộng hòa.'),
+    // ('Chiến thắng Điện Biên Phủ', '1954-05-07', 'Ngày 7 tháng 5 năm 1954, quân đội Việt Nam giành chiến thắng tại Điện Biên Phủ, kết thúc chiến tranh Đông Dương.'),
+    // ('Hiệp định Genève', '1954-07-21', 'Ngày 21 tháng 7 năm 1954, Hiệp định Genève được ký kết, chia đôi Việt Nam thành hai miền với giới tuyến tại vĩ tuyến 17.'),
+    // ('Chiến dịch Mậu Thân', '1968-01-30', 'Ngày 30 tháng 1 năm 1968, Quân Giải phóng miền Nam Việt Nam phát động cuộc tổng tiến công và nổi dậy trên toàn miền Nam.'),
+    // ('Giải phóng miền Nam', '1975-04-30', 'Ngày 30 tháng 4 năm 1975, quân đội Giải phóng miền Nam tiến vào Sài Gòn, giải phóng hoàn toàn miền Nam, thống nhất đất nước.'),
+    // ('Ngày thành lập Hội Liên hiệp Phụ nữ Việt Nam', '1930-10-20', 'Ngày 20 tháng 10 năm 1930, Hội Liên hiệp Phụ nữ Việt Nam được thành lập.'),
+    // ('Ngày nhà giáo Việt Nam', '1958-11-20', 'Ngày 20 tháng 11 hàng năm được chọn làm Ngày nhà giáo Việt Nam để tôn vinh các thầy cô giáo.'),
+    // ('Việt Nam gia nhập ASEAN', '1995-07-28', 'Ngày 28 tháng 7 năm 1995, Việt Nam chính thức gia nhập Hiệp hội các quốc gia Đông Nam Á (ASEAN).');
+
+
+    //get all events
+    public function getAllEvents(){
+        $sql = "SELECT * FROM event";
+        $db = new DB;
+        $db = $db->query($sql);
+        $db = $db->fetchAll(PDO::FETCH_ASSOC);
+        return $db;
+    }   
+
+    //thong bao cac su kien trong ngay
+    public function getEventsByDate($event_date){
+        $sql = "SELECT * FROM event WHERE event_date = '$event_date'";
+        $db = new DB;
+        $db = $db->query($sql);
+        $db = $db->fetchAll(PDO::FETCH_ASSOC);
+        return $db;
+    }
+
+    //thong bao su kien truoc 2 ngay
+    public function getEventsBeforeDate($event_date){
+        $sql = "SELECT * FROM event WHERE event_date = '$event_date'";
+        $db = new DB;
+        $db = $db->query($sql);
+        $db = $db->fetchAll(PDO::FETCH_ASSOC);
+        return $db;
+    }
 
 
 
