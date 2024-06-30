c8e8024f6547cab54dc11045739315b760b60623
guunyvukhi <gunnytuananh@gmail.com>
2024-07-01 01:13:35 +0700

    update fix loi + seach xong 50%

--- resources/view/Profile.php
+++ resources/view/Profile.php
@@ -367,5 +367,5 @@ $UserEvent = "";
         <?php include_once "postedlist.php";
         include 'footer.php'; ?>
 </body>
-
+<script lang="javascript" type="text/javascript" src="resources/js/Profile.js"></script>
 </html>
